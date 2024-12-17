6ee4dcc45ec92a00385acbbceaa2ef236f5c4162
t-schneids <tschneids1@icloud.com>
2024-12-17 14:18:07 -0500

    Create recommend.py
    Created original file

---
+++ recommend.py
@@ -0,0 +1,8 @@
+# 12/17/2024
+# Authors: Umer Ayub, Theodore Schneider
+# Purpose: To provide an interface for users to successfully
+#          get book recommendations based on previous reads
+
+
+
+# design/implementation plan
