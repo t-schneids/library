8b3503c1b33a8c9e63009ec11070037aae8caa7a
t-schneids <tschneids1@icloud.com>
2024-12-17 16:25:37 -0500

    BookManager Class Creation
    Created BookManager class. Also created initialization function for the class.

---
+++ bookManager.py
@@ -0,0 +1,39 @@
+# 12/17/2024
+# Authors: Umer Ayub, Theodore Schneider
+# Purpose: To provide an interface for users to successfully
+#          get book recommendations based on previous reads
+
+import requests
+import json
+
+class BookManager:
+    def __init__(self, db_file='books_db.json'):
+        """initialize a json file to act as a database for the user"""
+        self.db_file = db_file
+
+        try:
+            with open(self.db_file, 'r') as file:
+                self.books_db = json.load(file)
+        except:
+            self.books_db = {}
+
+
+# design/implementation plan
+
+# create a class that can export these functionalities:
+# function that provides recommendations -- it just asks for input
+    # def get_recommendations(self, book_title):
+# function that can add books that the user has read to the 'db'
+    # def add_book(self, book_title):
+# function that allows user to review books that they have already read
+    # def review_book(self, book_title, review):
+# function that allows interactive use of all of these functions
+    # def interactive(self):
+
+# PRIVATE FUNCTIONS: 
+    # def __init__(self, db_file='books_db.json'):
+    # def __save_db(self):
+
+
+
+
--- recommend.py
+++
@@ -1,8 +0,0 @@
-# 12/17/2024
-# Authors: Umer Ayub, Theodore Schneider
-# Purpose: To provide an interface for users to successfully
-#          get book recommendations based on previous reads
-
-
-
-# design/implementation plan
